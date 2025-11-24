6418bae50ac2bbd1f4e8e7b0792678ef2a6c8d1e
tarnishablec <tarnishablec@outlook.com>
2025-11-25 03:59:24 +0800

    Enhance service provider with default and custom builders
    Updated TServiceProvider to support custom service builders via a new template parameter. Added logic to differentiate between default and custom provider implementations, ensuring greater flexibility and extensibility.

--- Source/Banette/Pipeline/ServiceProvider.h
+++ Source/Banette/Pipeline/ServiceProvider.h
@@ -3,16 +3,38 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Banette/Core/Service.h"
 
 namespace Banette::Pipeline
 {
-	template <CService T>
+	template <CService T, typename ProviderT = void>
 	struct TServiceProvider
 	{
-		static TSharedPtr<T> GetService()
+		static inline TSharedPtr<T> Service = nullptr;
+
+		static TSharedPtr<T> BuildService()
 		{
 			unimplemented();
 			return nullptr;
+		};
+
+		static TSharedPtr<T> GetService()
+		{
+			if (Service.IsValid())
+			{
+				return Service;
+			}
+			
+			if constexpr (!std::is_same_v<ProviderT, void>)
+			{
+				Service = ProviderT::BuildService();
+			}
+			else
+			{
+				Service = BuildService();
+			}
+			
+			return Service;
 		};
 	};
 }
